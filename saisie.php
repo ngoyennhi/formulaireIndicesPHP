70d9b4af36e3a724777de41670ba9a04c19e11cf
YenNhiHTM <ngoyennhi.ho@gmail.com>
2022-01-27 21:41:34 +0100

    fini TP

---
+++ saisie.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // Toujours utiliser var_dump pour vérifier ce que l'on a
+    //var_dump($_POST);
+
+    $arrSaisi = filter_input(
+        INPUT_POST,
+        'saisie',
+        FILTER_DEFAULT,
+        FILTER_REQUIRE_ARRAY
+    );
+    if (isset($arrSaisi) && !empty($arrSaisi)) {
+        $nom = $arrSaisi['nom'];
+        $motDePass = $arrSaisi['mot_de_passe'];
+        $sexe = $arrSaisi['sexe'];
+        $photo = $arrSaisi['photo'];
+        $arrCouleurs = $arrSaisi['couleurs']; // array de couleurs
+        $langue = $arrSaisi['langue'];
+        $arrFruits = $arrSaisi['fruits']; // array de fruits
+        $commentaire = $arrSaisi['commentaire'];
+        $invisible = $arrSaisi['invisible'];
+        $soumettre = filter_input(INPUT_POST, 'soumettre');
+
+        //nom
+        if (isset($nom) && !empty($nom)) {
+            echo 'Nom = ' . $nom . '<br>';
+        } else {
+            echo 'Nom: inconnu';
+        };
+
+        //mot de passe
+        if (isset($motDePass) && !empty($motDePass)) {
+            echo 'Mot de passe = ' . $motDePass . '<br>';
+        } else {
+            echo 'Mot de passe : inconnu';
+        };
+
+        //sexe
+        if (isset($sexe) && !empty($sexe)) {
+            echo 'Sexe = ' . $sexe . '<br>';
+        } else {
+            echo 'Sexe : inconnu';
+        };
+
+        //photo
+        if (isset($photo) && !empty($photo)) {
+            echo 'Photo = ' . $photo . '<br>';
+        } else {
+            echo 'Photo : inconnu';
+        };
+
+        //Couleur
+        if (isset($arrCouleurs) && !empty($arrCouleurs)) {
+            echo 'Couleur = <br>';
+            if ($arrCouleurs['bleu'] == 'on') {
+                echo 'blue = ' . $arrCouleurs['bleu'] . '<br>';
+            }
+            if ($arrCouleurs['blanc'] == 'on') {
+                echo 'blanc = ' . $arrCouleurs['blanc'] . '<br>';
+            }
+            if ($arrCouleurs['rouge'] == 'on') {
+                echo 'rouge = ' . $arrCouleurs['rouge'] . '<br>';
+            }
+            if ($arrCouleurs['nesaitpas'] == 'on') {
+                echo 'nesaitpas = ' . $arrCouleurs['nesaitpas'] . '<br>';
+            }
+        } else {
+            echo 'Couleurs : inconnu';
+        };
+
+        //langue
+        if (isset($langue) && !empty($langue)) {
+            echo 'Langue = ' . $langue . '<br>';
+        } else {
+            echo 'Langue : inconnu';
+        };
+
+        // Fruits        
+        $arrFruits = $arrSaisi['fruits']; // array de fruits
+        if (isset($arrFruits)&&!empty($arrFruits)) {
+            $lengthArr = count($arrFruits);
+            for($i = 0;$i < $lengthArr;$i++){
+                echo $i.' = '.$arrFruits[$i].'<br>';
+            };
+        } else {
+            echo 'Fruits : inconnu';
+        };
+
+
+        //Commentaire
+        if (isset($commentaire) && !empty($commentaire)) {
+            echo 'Commentaire = ' . $commentaire . '<br>';
+        } else {
+            echo 'Commentaire : inconnu';
+        }
+
+        //Invisible
+        if (isset($invisible) && !empty($invisible)) {
+            echo 'Invisible = ' . $invisible . '<br>';
+        } else {
+            echo 'Invisible : inconnu';
+        }
+
+        // Soumettre
+        if (isset($soumettre) && !empty($soumettre)) {
+            echo 'Soumettre = ' . $soumettre . '<br>';
+        } else {
+            echo 'Soumettre : inconnu';
+        }
+    } else {
+        echo 'inconnu!';
+    }
+    ?>
+</body>
+</html>
